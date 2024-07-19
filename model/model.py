079ba9f6e4303d206fda20eb1e209c7822843ff2
Tewodros Nibret Adal <tewodrosnibretadal2012@gamil.com>
2024-07-19 15:43:48 +0300

    some pages added

--- model/model.py
+++ model/model.py
@@ -11,16 +11,9 @@ from langchain_google_vertexai import ChatVertexAI
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from model import GOOGLE_API_KEY, LANGCHAIN_API_KEY
 
-# Load, chunk and index the contents of the blog.
+# Load, chunk and index the contents of the pages.
 
-# # loader = WebBaseLoader(
-#     web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
-#     bs_kwargs=dict(
-#         parse_only=bs4.SoupStrainer(
-#             class_=("post-content", "post-title", "post-header")
-#         )
-#     ),
-# )
+
 
 
 llm = ChatGoogleGenerativeAI(
@@ -38,7 +31,13 @@ web_paths = [
     "https://awachsacco.com/index.php/saving/",
     "https://awachsacco.com/index.php/training-and-consultancy/",
     "https://awachsacco.com/index.php/loan/",
+    "https://awachsacco.com/services",
+    "https://awachsacco.com/about-awach",
+    "https://awachsacco.com/mr-zerihun-sheleme-0",
+    "https://awachsacco.com/requirement-membership"
 ]
+
+
 
 loader = WebBaseLoader(
     web_paths=web_paths,
@@ -48,6 +47,7 @@ loader = WebBaseLoader(
         )
     ),
 )
+
 docs = loader.load()
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
